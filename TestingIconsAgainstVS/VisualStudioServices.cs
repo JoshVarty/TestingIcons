3dbea3aa4fcc96f8540a3c877daec7998a1da7bc
Josh Varty <joshvarty@gmail.com>
2015-07-06 03:10:48 -0400

    Including sample project to demo our custom icons.

---
+++ TestingIconsAgainstVS/VisualStudioServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.LanguageServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingIconsAgainstVS
+{
+    public static class VisualStudioServices
+    {
+        public static VisualStudioWorkspace Workspace { get; set; }
+
+    }
+}
